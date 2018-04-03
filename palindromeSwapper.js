7f32de178f8c5b684af3be09546189e13b4c036d
asepridwan1989 <aseprmail@gmail.com>
2018-04-03 11:33:15 +0700

    done

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -1,4 +1,4 @@
-/**
+/*
  * @function palindromeSwapper
  * menukar 2 huruf berurutan dari n = 0 sampai n-1
  * contoh: makan => amkan, mkaan, maakn, makna
@@ -6,11 +6,12 @@
  * @returns {boolean} true bila kata yang di-swap adalah palindrom
  */
 function palindromeSwapper(str) {
-  let newStr = '';
-  for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
+  if (isPalindrome(str)) return true;
+  for (var i = 0; i < str.length-1; i += 2) {
+    let newStr = '';
+    for (var j = 0; j < str.length; j++) {
       if (i === j) {
-        newStr += str[j++];
+        newStr += str[j+1];
         newStr += str[j];
         j++;
       } else {
@@ -22,15 +23,17 @@ function palindromeSwapper(str) {
   return false;
 }
 
-/**
+/*
  * @function isPalindrome
  * Cek apabila kata merupakan palindromeSwapper
  * @param {string} str - input kata yang dicek bila palindrom
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  let strPalindrome = str.split('').reverse().join('')
+  console.log('masuk sini: ', str, 'palindrome : ', strPalindrome)
+  if (strPalindrome === str)return true;
+  return false;
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
