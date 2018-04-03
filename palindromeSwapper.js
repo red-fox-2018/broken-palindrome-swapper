33f2d8fb236576a4fd13b9372139112336179413
Natascha Lamsu <tazcha15@gmail.com>
2018-04-03 12:21:21 +0700

    palindrome swapper

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -6,20 +6,24 @@
  * @returns {boolean} true bila kata yang di-swap adalah palindrom
  */
 function palindromeSwapper(str) {
-  let newStr = '';
-  for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
-      if (i === j) {
-        newStr += str[j++];
-        newStr += str[j];
-        j++;
-      } else {
-        newStr += str[j];
+  if(isPalindrome(str)) {
+    return true
+  } else {
+    for (var i = 0; i < str.length-1; i++) {
+      let newStr = '';
+      for (var j = 0; j < str.length-1; j++) {
+        if (i === j) {
+          // console.log(newStr);
+          newStr += str[j+1];
+          newStr += str[j];
+        } else {
+          newStr += str[j];
+        }
       }
+      if (isPalindrome(newStr)) return true;
     }
-    if (isPalindrome(newStr)) return true;
+    return false;
   }
-  return false;
 }
 
 /**
@@ -29,8 +33,11 @@ function palindromeSwapper(str) {
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  if (str.split('').reverse().join('') === str) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
