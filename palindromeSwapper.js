c008b33a68fd59cc9ef79ca26240a04da1c2b739
yasirmaulana <yasir.maulana@gmail.com>
2018-04-18 10:27:43 +0700

    late push - broken palindrome swapper

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -7,19 +7,29 @@
  */
 function palindromeSwapper(str) {
   let newStr = '';
-  for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
-      if (i === j) {
-        newStr += str[j++];
-        newStr += str[j];
-        j++;
-      } else {
-        newStr += str[j];
-      }
-    }
-    if (isPalindrome(newStr)) return true;
+  if (isPalindrome(str)){
+    debugger
+    return true;
   }
-  return false;
+  else {
+    debugger
+    for (let i = 0; i < str.length; i++) {
+      debugger
+      for (let j = 0; j < str.length; j++) {
+        debugger
+        if (i === j) {
+          debugger
+          newStr += str[j+1];
+          newStr += str[j];
+          j++;
+        } else {
+          newStr += str[j];
+        }
+      }
+      if (isPalindrome(newStr)) return true
+    }
+    return false;
+  }
 }
 
 /**
@@ -29,8 +39,12 @@ function palindromeSwapper(str) {
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  if (str.split('').reverse().join('') === str){
+    return true
+  } 
+  else {
+    return false
+  }  
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
